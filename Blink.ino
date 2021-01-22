f542138f07d86229727a4c04a9f96adcff8d904c
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2021-01-22 10:37:29 -0800

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,26 @@
+//int LED = 13;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(30);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(30);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(30);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(30);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(3000);
+}
